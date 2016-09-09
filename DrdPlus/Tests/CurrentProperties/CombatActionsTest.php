de0a226c19b8b9012a23e15fa0d172785c9d6ba1
jaroslavtyc <mail@jaroslavtyc.com>
2016-09-09 15:05:38 +0200

    Combat actions can be grouped to a compatibility validated container

---
+++ DrdPlus/Tests/CurrentProperties/CombatActionsTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace DrdPlus\CurrentProperties;
+
+use DrdPlus\Codes\CombatActions\CombatActionCode;
+use DrdPlus\Tables\Actions\CombatActionsCompatibilityTable;
+use Granam\Tests\Tools\TestWithMockery;
+
+class CombatActionsTest extends TestWithMockery
+{
+    /**
+     * @test
+     */
+    public function I_can_use_it()
+    {
+        $combatActions = new CombatActions(
+            $inputActions = [CombatActionCode::getIt(CombatActionCode::BLINDFOLD_FIGHT)],
+            $this->createCombatActionsCompatibilityTable($inputActions, true /* compatible */)
+        );
+        self::assertCount(1, $combatActions);
+        self::assertSame($inputActions, $combatActions->getCombatActionCodes());
+    }
+
+    /**
+     * @param array $expectedActionsToCombine
+     * @param bool $compatible
+     * @return \Mockery\MockInterface|CombatActionsCompatibilityTable
+     */
+    private function createCombatActionsCompatibilityTable(array $expectedActionsToCombine, $compatible)
+    {
+        $combatActionsCompatibilityTable = $this->mockery(CombatActionsCompatibilityTable::class);
+        $combatActionsCompatibilityTable->shouldReceive('canCombineTwoActions')
+            ->with(\Mockery::type(CombatActionCode::class), \Mockery::type(CombatActionCode::class))
+            ->andReturnUsing(function (CombatActionCode $someAction, CombatActionCode $anotherAction) use ($expectedActionsToCombine, $compatible) {
+                self::assertTrue(in_array($someAction, $expectedActionsToCombine, true));
+                self::assertTrue(in_array($anotherAction, $expectedActionsToCombine, true));
+
+                return $compatible;
+            });
+
+        return $combatActionsCompatibilityTable;
+    }
+}
